45affed599cac7240fa1e7b6d24c1e91f70c662f
Lu <lu492163059@163.com>
2024-08-04 16:48:33 +0800

    背景修改

--- src/leading_end/JS/main_back.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('changeBackgroundBtn').addEventListener('click', function() {
-    // 更改背景颜色
-    document.body.style.backgroundColor = '#f0f0f0';
-    // 或者更改背景图片
-    // document.body.style.backgroundImage = "url('path/to/your-background-image.jpg')";
-    // document.body.style.backgroundSize = 'cover';
-    // document.body.style.backgroundPosition = 'center';
-});
