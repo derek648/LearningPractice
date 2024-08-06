68efb6fad3cef4bb1cac11456c30c3977219699f
derek648 <100399440+derek648@users.noreply.github.com>
2024-08-06 08:58:04 +0800

    Add files via upload

---
+++ wenxin/target/wenxin/js/gzj/load/init.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    //将gzj的html页面模块,css,js发送到主页面
+    getPageInfo("html/gzj/leftMiddleBar.html","leftMiddleBar_gzj");
+    loadCSS("css/gzj/leftMiddlePage/hoverStyle.css")
+    loadCSS("css/gzj/leftMiddlePage/leftMiddlePage.css")
+    loadScript("js/gzj/leftMiddleBar/historicalDialog_gzj.js")
+
+    //将gyt的html页面模块,css,js发送到主页面
+    getPageInfo("html/gyt/chatArea.html","dialogBox_gyt");
+    loadCSS("css/gyt/Style.css")
+
+    //将lgc的html页面模块,css,js发送到主页面
+    getPageInfo("html/lgc/main_back.html","mainBack_lgc");
+    loadCSS("css/lgc/main_back/main_back.css")
+}
